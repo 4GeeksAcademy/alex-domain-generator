39417f540fd079f868a9e17eee1dd5efa1036d2e
Alexander Morales <alexandermorales.n1@gmail.com>
2025-03-24 15:14:44 +0000

    refactor for loop

--- src/app.js
+++ src/app.js
@@ -13,23 +13,13 @@ window.onload = function() {
 
   let phrase = [pronoun, adj, noun]
 
-  for (let i = 0 ; i < pronoun.length ; i ++) {
-    console.log(`${phrase[0][1]}${phrase[1][1]}${phrase[2][i]}.com`)
-  }
-  
-    for (let j = 0 ; j < pronoun.length ; j ++) {
-      console.log(`${phrase[0][1]}${phrase[1][0]}${phrase[2][j]}.com`)
-
+    for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(`${pronoun[i]}${adj[j]}${noun[k]}.com`);
+      }
     }
-      for (let k = 0 ; k < adj.length ; k ++) {
-          console.log(`${phrase[0][0]}${phrase[1][1]}${phrase[2][k]}.com`)
-        }
-
-        for (let l = 0 ; l < noun.length ; l ++) {
-          // console.log(`${phrase[0][i]}${phrase[1][i]}${phrase[2][l]}.com`)
-          console.log(`${phrase[0][0]}${phrase[1][0]}${phrase[2][l]}.com`)
-
-        }
+  }
 
 };
 
